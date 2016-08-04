55b80c386b212185438a3b36582fb65aa468849e
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-08-04 19:07:02 +0200

    La SDL est en place, j'ai aussi trouver le moyen d'ecrire un pixel dans l'equivalent des images de la mlx. Je vais pouvoir commencer a travailler serieusement !

--- sources/main.c
+++ sources/main.c
@@ -9,9 +9,44 @@
 //
 
 #include "wolf3d.h"
+#include "sdl.h"
 
 int						main(void)
 {
-	ft_putendl("Hello World\n");
+	t_bool				on;
+	SDL_Window			*window;
+	SDL_Renderer		*renderer;
+	SDL_Event			events;
+
+	on = TRUE;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		dprintf(STDERR_FILENO, "SDL_Init error: %s\n", SDL_GetError());
+		SDL_Quit();
+		return (EXIT_FAILURE);
+	}
+	SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+	if (!window)
+	{
+		dprintf(STDERR_FILENO, "SDL_CreateWindowAndRenderer error: %s\n", SDL_GetError());
+		SDL_Quit();
+		return (EXIT_FAILURE);
+	}
+	SDL_SetWindowTitle(window, "SDL TEST");
+	//DRAW RECTANGLE
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+	for (int x = 0; x < 800; x++)
+		for (int y = 0; y < 600; y++)
+			SDL_RenderDrawPoint(renderer, x, y);
+	SDL_RenderPresent(renderer);
+	while (on)
+	{
+		SDL_WaitEvent(&events);
+		if (events.type == SDL_QUIT)
+			on = FALSE;
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return (EXIT_SUCCESS);
 }
