217b7a1b19bcc3373e940dafee2de10e1576d754
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-08-11 12:04:18 +0200

    Je vais laisser cette branche de cote parce que j'hesite a refaire un algo de raycating pour y voir plus claire dans ce probleme de textures (mes algo sont chelou pour le moment)

--- sources/logic/raycasting.c
+++ sources/logic/raycasting.c
@@ -21,9 +21,14 @@ void 			raycasting(t_sdl *sdl, t_cam *cam)
 	t_ray		ray;
 	t_dda		dda_n;
 	double 		pwd;
+	//TODO DEGAGER TEXTURE
+	SDL_Surface	*texture;
 
 	x = -1;
 	draw_background(sdl);
+	//TODO DEGAGER TEXTURE
+	if (!(texture = IMG_Load(GREYSTONE_TEXTURE)))
+		return ;
 	while (++x < WIN_SIZE_X)
 	{
 		init_ray(&ray, cam, x);
@@ -35,7 +40,6 @@ void 			raycasting(t_sdl *sdl, t_cam *cam)
 		//draw_wall(sdl, x, pwd);
 
 		//TROUBLE HERE
-
 		int 		line_height;
 		int 		start;
 		int 		end;
@@ -46,40 +50,34 @@ void 			raycasting(t_sdl *sdl, t_cam *cam)
 		end = line_height / 2 + WIN_SIZE_Y / 2;
 		end = (end >= WIN_SIZE_Y) ? WIN_SIZE_Y - 1 : end;
 
-		//NO REALLY HERE !
-		static SDL_Surface	*tex;
+		//TROUBLE REALLY BEGIN HERE !
 		double		wall_x;
+		int			tex_x;
 
-		if (!tex)
-			tex = IMG_Load(GREYSTONE_TEXTURE);
 		wall_x = (!side) ? ray.pos.x + pwd * ray.dir.y
 						 : ray.pos.x + pwd * ray.dir.x;
 		wall_x -= floor(wall_x);
-
-		int			tex_x;
-
-		tex_x = (int)(wall_x * tex->w);
+		tex_x = (int)(wall_x * texture->w);
 		if (!side && ray.dir.x > 0)
-			tex_x = tex->w - tex_x - 1;
+			tex_x = texture->w - tex_x - 1;
 		if (side && ray.dir.x < 0)
-			tex_x = tex->w - tex_x - 1;
-
+			tex_x = texture->w - tex_x - 1;
 		for (int y = start; y < end; y++)
 		{
 			int 		tex_y;
 			Uint32		color;
-			Uint8 	pxl[4];
+			Uint8 		pxl[4];
 
 			tex_y = (y * 2 - WIN_SIZE_Y + line_height) * 32 / line_height;
-			SDL_LockSurface(tex);
-			color = get_pixel(tex, tex_x, tex_y);
-			SDL_UnlockSurface(tex);
-			SDL_GetRGBA(color, tex->format, &(pxl[0]), &(pxl[1]), &(pxl[2]), &(pxl[3]));
+			SDL_LockSurface(texture);
+			color = get_pixel(texture, tex_x, tex_y);
+			SDL_UnlockSurface(texture);
+			SDL_GetRGBA(color, texture->format, &(pxl[0]), &(pxl[1]), &(pxl[2]), &(pxl[3]));
 			SDL_SetRenderDrawColor(sdl->renderer, pxl[0], pxl[1], pxl[2], pxl[3]);
 			SDL_RenderDrawPoint(sdl->renderer, x, y);
 		}
-
 		//END TROUBLE
 	}
 	SDL_RenderPresent(sdl->renderer);
+	SDL_FreeSurface(texture);
 }
