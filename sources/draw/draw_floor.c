33cf2f906f410aea5f979b86ce82f46c9db0b579
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-09-08 15:43:43 +0200

    norme

--- sources/draw/draw_floor.c
+++ sources/draw/draw_floor.c
@@ -6,11 +6,12 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/08 13:16:17 by alegent           #+#    #+#             */
-/*   Updated: 2016/09/08 13:41:14 by alegent          ###   ########.fr       */
+/*   Updated: 2016/09/08 15:42:12 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+#include "floor.h"
 
 static void				draw_sky_texture(t_all *all, t_coor *texture)
 {
@@ -42,47 +43,52 @@ static void				draw_floor_texture(t_all *all, t_coor *texture)
 			(int)all->game.iter.x, (int)all->game.iter.y);
 }
 
+static void				ft_norme(t_all *all, t_floor *fall)
+{
+	while (++(all->game.iter.y) < all->sdl.win.height)
+	{
+		fall->current_dist =
+			all->sdl.win.height / (2 * all->game.iter.y - all->sdl.win.height);
+		fall->weight = (fall->current_dist - fall->dist_player)
+			/ (fall->dist_wall - fall->dist_player);
+		fall->current_floor.x = fall->weight * fall->floor.x
+			+ (1 - fall->weight) * all->game.cam.pos.x;
+		fall->current_floor.y = fall->weight * fall->floor.y
+			+ (1 - fall->weight) * all->game.cam.pos.y;
+		fall->texture.x = (int)(fall->current_floor.x * all->sdl.floor->w)
+			% (int)all->sdl.floor->w;
+		fall->texture.y = (int)(fall->current_floor.y * all->sdl.floor->h)
+			% (int)all->sdl.floor->h;
+		draw_floor_texture(all, &fall->texture);
+		draw_sky_texture(all, &fall->texture);
+	}
+}
+
 void					draw_floor(t_all *all)
 {
-	t_coor				floor;
-	double				dist_wall;
-	double				dist_player;
-	double				current_dist;
-	double				weight;
-	t_coor				current_floor;
-	t_coor				texture;
+	t_floor				fall;
 
 	if (!(all->game.dda.s) && all->game.ray.dir.x > 0)
 	{
-		floor.x = all->game.dda.map.x;
-		floor.y = all->game.dda.map.y + all->game.dda.wall_x;
+		fall.floor.x = all->game.dda.map.x;
+		fall.floor.y = all->game.dda.map.y + all->game.dda.wall_x;
 	}
 	else if (!(all->game.dda.s) && all->game.ray.dir.x < 0)
 	{
-		floor.x = all->game.dda.map.x + 1;
-		floor.y = all->game.dda.map.y + all->game.dda.wall_x;
+		fall.floor.x = all->game.dda.map.x + 1;
+		fall.floor.y = all->game.dda.map.y + all->game.dda.wall_x;
 	}
 	else if (all->game.dda.s && all->game.ray.dir.y > 0)
 	{
-		floor.x = all->game.dda.map.x + all->game.dda.wall_x;
-		floor.y = all->game.dda.map.y;
+		fall.floor.x = all->game.dda.map.x + all->game.dda.wall_x;
+		fall.floor.y = all->game.dda.map.y;
 	}
 	else
 	{
-		floor.x = all->game.dda.map.x + all->game.dda.wall_x;
-		floor.y = all->game.dda.map.y + 1;
+		fall.floor.x = all->game.dda.map.x + all->game.dda.wall_x;
+		fall.floor.y = all->game.dda.map.y + 1;
 	}
-	dist_wall = all->game.dda.pwd;
-	dist_player = 0;
-	while (++(all->game.iter.y) < all->sdl.win.height)
-	{
-		current_dist = all->sdl.win.height / (2 * all->game.iter.y - all->sdl.win.height);
-		weight = (current_dist - dist_player) / (dist_wall - dist_player);
-		current_floor.x = weight * floor.x + (1 - weight) * all->game.cam.pos.x;
-		current_floor.y = weight * floor.y + (1 - weight) * all->game.cam.pos.y;
-		texture.x = (int)(current_floor.x * all->sdl.floor->w) % (int)all->sdl.floor->w;
-		texture.y = (int)(current_floor.y * all->sdl.floor->h) % (int)all->sdl.floor->h;
-		draw_floor_texture(all, &texture);
-		draw_sky_texture(all, &texture);
-	}
+	fall.dist_wall = all->game.dda.pwd;
+	fall.dist_player = 0;
+	ft_norme(all, &fall);
 }
